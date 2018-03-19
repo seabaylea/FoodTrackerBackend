e24febc84e1c32c118aa6016fc53ef1283d1781e
Chris Bailey <baileyc@uk.ibm.com>
2018-03-19 15:32:20 -0700

    Update Application.swift

--- Application.swift
+++ Application.swift
@@ -4,10 +4,8 @@ import LoggerAPI
 import Configuration
 import CloudEnvironment
 import KituraContracts
-import Health
 
 public let projectPath = ConfigurationManager.BasePath.project.path
-public let health = Health()
 
 public class App {
     let router = Router()
@@ -17,14 +15,9 @@ public class App {
     private var mealStore: [String: Meal] = [:]
 
     public init() throws {
-        // Run the metrics initializer
-        initializeMetrics(router: router)
     }
 
     func postInit() throws {
-        // Endpoints
-        initializeHealthRoutes(app: self)
-     
         // Routes that the application will respond on:
         // POST on /meals calls storeHandler()
         // GET  on /meals calls loadHandler()   
